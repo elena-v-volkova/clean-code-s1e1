c887b38c9a197eda0ae8cefb8624d78ab1c7b3c2
Vl Lena <tg @vlpro>
2024-12-17 14:37:36 +0300

    refactor: add alt description for img according to rule 1.2. Media alternatives

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var createNewTaskElement=function(taskString){
     //button.delete
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.alt = "delete button"
 
     label.innerText=taskString;
     label.className='todo__task-label';
